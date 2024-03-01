f7346cdafe6e91f7809671015d834ea57dbd48cd
gurnemat <151676705+gurnemat@users.noreply.github.com>
2024-03-01 11:13:30 -0800

    intial commit

--- src/script.js
+++ src/script.js
@@ -1,21 +1,58 @@
-// write your JavaScript here
 // DON'T remove the below import
-import "./styles.css";
+// import "./style.css";
 
-const showBtn = document.querySelector(".open-btn");
-const section = document.querySelector("section");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".close-card span");
-const cancelBtn = document.querySelector(".btns button:first-child");
-showBtn.addEventListener("click", () => {
-  section.classList.add("active");
-});
-overlay.addEventListener("click", () => section.classList.remove("active"));
+const showFeed = document.querySelector(".show-feed");
+const feedback = document.querySelector(".feedback");
+const thanks = document.querySelector(".thanks");
+const backdoor = document.querySelector(".backdoor");
+const submit = document.querySelector("#submit");
+const ratings = document.querySelector("#ratings");
+let score = document.querySelectorAll(".score button");
+let result = "";
 
-closeBtn.addEventListener("click", () => {
-  section.classList.remove("active");
+score.forEach((items, index) => {
+  items.addEventListener("click", (item) => {
+    ratings.textContent = `Your give rate is ${items.value}`;
+    result = items.value;
+  });
 });
 
-cancelBtn.addEventListener("click", () => {
-  section.classList.remove("active");
+// Submit feedback
+submit.addEventListener("click", () => {
+  if (result === "") {
+    alert("You forgot to give feedback ");
+  } else {
+    showLayout(thanks, feedback);
+  }
 });
+
+// show feedback
+document.querySelector("#showFeed").addEventListener("click", () => {
+  showLayout(feedback, showFeed, backdoor);
+});
+
+// close feedback with click background
+backdoor.addEventListener("click", () => {
+  showLayout(showFeed, feedback);
+});
+
+// close feed with click cancel or close (X)
+document.querySelector(".close").addEventListener("click", () => {
+  showLayout(showFeed, feedback);
+});
+document.querySelector(".cancel").addEventListener("click", () => {
+  showLayout(showFeed, feedback);
+});
+
+// back to home
+document.querySelector("#home").addEventListener("click", () => {
+  showLayout(showFeed, thanks);
+});
+
+function showLayout(show, hide, backdor) {
+  show.classList.add("active");
+  hide.classList.remove("active");
+  backdor
+    ? backdoor.classList.add("active")
+    : backdoor.classList.remove("active");
+}
